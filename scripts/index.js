62d5a12363d77abd758092b568887410e713d49c
WNjihia <dorisn172@gmail.com>
2019-11-14 13:38:34 +0300

    restrict input to digits only

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,31 @@
-var input = document.getElementById("phoneNumber");
-input.addEventListener("keyup", function(event){
-  var value = event.currentTarget.value;
-  //add checks
-})
+window.onload = function() {
+  var input = document.getElementById("phoneNumber");
+  var filter = [];
+
+  //adding keycodes to filter - numeric keys/numpad
+  const keypadZero = 48;
+  const numpadZero = 96;
+
+  for(var i = 0; i <= 9; i++){
+    filter.push(i + keypadZero);
+    filter.push(i + numpadZero);
+  }
+
+  filter.push(8);     //backspace
+  filter.push(9);     //tab
+  filter.push(46);    //delete
+  filter.push(37);    //left arrow
+  filter.push(39);    //right arrow
+
+  input.addEventListener("keyup", function(event){
+    var value = event.currentTarget.value;
+  })
+
+  // ensure phone number contains only 0-9
+  input.addEventListener("keydown", function(event){
+    if (filter.indexOf(event.keyCode) < 0){
+      event.preventDefault();
+      return false;
+    }
+  })
+}
