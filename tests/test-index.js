362ac4856ad8a59cf6ca7be2bbefb8673eb81fc5
WNjihia <dorisn172@gmail.com>
2019-11-15 08:27:14 +0300

    add skeleton for tests

---
+++ tests/test-index.js
@@ -0,0 +1,36 @@
+var assert = require('assert')
+var chai = require('chai')
+var expect = chai.expect()
+var should = chai.should()
+
+
+describe('Unit Tests', function(){
+  it('should have ten numbers', function(){
+
+  });
+
+  it('should not include letters', function(){
+
+  });
+
+  //(NXX-NXX-XXXX)
+  it('N should be digits between 2 and 9', function(){
+
+  });
+
+  it('should not include country code', function(){
+
+  });
+
+  it('should remove punctuation on valid numbers', function(){
+
+  });
+
+});
+
+
+describe('Integration Test', function(){
+  it('should display correctly formatted number', function(){
+
+  });
+});
